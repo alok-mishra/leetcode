61945f1a4eb803189cb16f8e458c79375d11fdc5
Alok Mishra <alok@alokmishra.com>
2023-11-20 23:56:35 -0500

    initial leetcode question

---
+++ 1814 - count nice pairs.js
@@ -0,0 +1,12 @@
+function countNicePairs(nums) {
+    var rev = function (n) { return parseInt(n.toString().split('').reverse().join('')); };
+    var countMap = {};
+    var nice = 0;
+    nums.forEach(function (num) {
+        console.log("hi");
+    });
+    return 2;
+}
+;
+countNicePairs([42, 11, 1, 97]);
+countNicePairs([13, 10, 35, 24, 76]);
